44dabc9f3c08b4cea10fe6567e23278c31fcb1a7
Lê Công Danh <lcngdanh63@gmail.com>
2024-05-21 01:19:28 +0700

    continue coding

--- backend/main-api/blueprints/auth.py
+++ backend/main-api/blueprints/auth.py
@@ -1,9 +1,14 @@
 from flask import Blueprint, jsonify, request
+import pymongo
 import uuid
+
+DB_NAME = "archiver_database"
+client = pymongo.MongoClient('localhost', 27017)
+db = client[DB_NAME]
 
 auth = Blueprint('auth', __name__)
 
-def check_session(session_id):
+def start_session(session_id):
 	return True
 
 @auth.route('/')
@@ -13,28 +18,35 @@ def default():
 @auth.route('/login', methods=['GET','POST'])
 def login():
 	RESPONSE = {}
-	# username = request.form.get("username")
 	username = request.args.get("username")
-	# password = request.form.get("password")
 	password = request.args.get("password")
-	if (username == None or password == None):
+	user = db["users"]
+	query = {"username": username, "password": password}
+	user_query = user.find_one(query)
+	session_id = str(uuid.uuid4())
+	query = {"session_id": session_id}
+	if (user_query == None):
 		RESPONSE["success"] = False
-		RESPONSE["msg"] = "Wrong username or password"
+		RESPONSE["msg"] = "Wrong username or password."
 	else:
 		RESPONSE["success"] = True
-		RESPONSE["msg"] = "Login successful"
-		RESPONSE["session_id"] = str(uuid.uuid4())
+		RESPONSE["msg"] = "Login successful."
+		RESPONSE["session_id"] = session_id
+		user.insert_one(query)
 	return jsonify(RESPONSE)
 
 @auth.route('/logout', methods=['GET','POST'])
 def logout():
 	RESPONSE = {}
-	# session_id = request.form.get("session_id")
 	session_id = request.args.get("session_id")
-	if (session_id == None):
+	user = db["users"]
+	query = {"session_id": session_id}
+	session_id_query = user.find_one(query)
+	if (session_id_query == None):
 		RESPONSE["success"] = False
-		RESPONSE["msg"] = "Session ID is empty"
+		RESPONSE["msg"] = "Session ID is empty."
 	else:
+		user.delete_one(query)
 		RESPONSE["success"] = True
-		RESPONSE["msg"] = "Logout successful"
+		RESPONSE["msg"] = "Logout successful."
 	return jsonify(RESPONSE)

9bdb6b3c87cd597fbf824d04eaa5bde10fb49aa2
rostectg <hi>
2024-05-22 11:00:24 -0700

    fix indentation Register.py

--- backend/main-api/blueprints/register.py
+++ backend/main-api/blueprints/register.py
@@ -15,55 +15,61 @@ db = client[DB_NAME]
 # route check user is valid or not
 @register.route('/check', methods= ['POST'])
 def check():
-  username = request.json.get('username')  
-  if (User().is_available(username)):
-    return jsonify({
-      "success": True,
-      "message": "Username is avalable."
-      }), 200
-  else:
-    return jsonify({
-      "success": False,
-      "message": "Username is not available."
-      }), 200
+	username = request.json.get('username')	
+	if (User().is_available(username)):
+	return jsonify({
+		"success": True,
+		"message": "Username is avalable."
+		}), 200
+	else:
+	return jsonify({
+		"success": False,
+		"message": "Username is not available."
+		}), 200
 
 # route do register
 @register.route('/do_register', methods= ['POST'])
 def do_register():
-  return User().do_register()
+	return User().do_register()
 
 class User:
-  def is_available(self, username):
-    entry = db.user.find_one({"username": username})
-    return not entry
+	def is_available(self, username):
+	entry = db.user.find_one({"username": username})
+	return not entry
 
-  def do_register(self):
-    # Create user object and get data from JSON
-    user = {
-    "user_id": str(uuid.uuid4()),
-    "email": request.json.get('email', ""),
-    "username": request.json.get('username'),
-    "password": request.json.get('password'),
-    }
+	def do_register(self):
+	# Create user object and get data from JSON
+	user = {
+		"user_id": str(uuid.uuid4()),
+		"email": request.json.get('email', ""),
+		"username": request.json.get('username'),
+		"password": request.json.get('password'),
+	}
 
-    # check if password is more than = 8 character
-    if len(user['password']) < 8:
-      return jsonify({
-        "success": False,
-        "message": "Password is less than 8 chacracter!"
-         }), 200
-    
-    # Encrypt the password
-    user['password'] = pbkdf2_sha256.encrypt(user['password'])
+	if not self.is_available(user["username"]):
+		return jsonify({
+			"success": False,
+			"message": "Username is not available."
+		}), 200
 
-    # Insert user to mongodb
-    if db.user.insert_one(user):
-      return jsonify({
-        "success": True,
-         "message": "Signup Successful!"
-         }), 200
-    
-    return jsonify({
-      "success": False,
-      "message": "Signup failed!"
-      }), 200
+	# check if password is more than = 8 character
+	if len(user['password']) < 8:
+		return jsonify({
+			"success": False,
+			"message": "Password is less than 8 chacracter!"
+		}), 200
+	
+	# Encrypt the password
+	user['password'] = pbkdf2_sha256.encrypt(user['password'])
+
+	# Insert user to mongodb
+	if db.user.insert_one(user):
+		return jsonify({
+			"success": True,
+			"message": "Signup Successful!"
+		}), 200
+	
+	return jsonify({
+		"success": False,
+		"message": "Signup failed!"
+	}), 200

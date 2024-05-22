fd78f8038098c96855c1a8e3c373749c344097eb
rostectg <hi>
2024-05-21 21:45:31 -0700

    fix: auth

--- backend/main-api/blueprints/auth.py
+++ backend/main-api/blueprints/auth.py
@@ -1,9 +1,11 @@
 from flask import Blueprint, jsonify, request
 import pymongo
 import uuid
+from passlib.hash import pbkdf2_sha256
 
+MONGO_CONNECTION_STRING = "mongodb://root:Passw0rd321@mongo:27017/"
 DB_NAME = "archiver_database"
-client = pymongo.MongoClient('localhost', 27017)
+client = pymongo.MongoClient(MONGO_CONNECTION_STRING)
 db = client[DB_NAME]
 
 auth = Blueprint('auth', __name__)
@@ -18,22 +20,28 @@ def default():
 @auth.route('/login', methods=['GET','POST'])
 def login():
 	RESPONSE = {}
-	username = request.args.get("username")
-	password = request.args.get("password")
-	user = db["users"]
-	query = {"username": username, "password": password}
-	user_query = user.find_one(query)
-	session_id = str(uuid.uuid4())
-	query = {"session_id": session_id}
-	if (user_query == None):
-		RESPONSE["success"] = False
-		RESPONSE["msg"] = "Wrong username or password."
-	else:
-		RESPONSE["success"] = True
-		RESPONSE["msg"] = "Login successful."
-		RESPONSE["session_id"] = session_id
-		user.insert_one(query)
-	return jsonify(RESPONSE)
+	username = request.json.get("username")
+	password = request.json.get("password")
+	user_collection = db["users"]
+	query_result = user_collection.find_one({"username": username})
+	if (query_result):
+		if (pbkdf2_sha256.verify(password, query_result["password"])):
+			session_id = str(uuid.uuid4())
+			query = {"session_id": session_id}
+
+			return jsonify({
+				"success": True,
+				"msg": "Login successful."
+			})
+				RESPONSE["success"] = True
+				RESPONSE["msg"] = "Login successful."
+				RESPONSE["session_id"] = session_id
+				user.insert_one(query)
+
+	return jsonify({
+		"success": False,
+		"msg": "Wrong username or password."
+	})
 
 @auth.route('/logout', methods=['GET','POST'])
 def logout():

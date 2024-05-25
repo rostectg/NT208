8f75814e9551b2edbe6919317a6291eccd2c94fc
rostectg <hi>
2024-05-25 14:12:20 -0700

    generate session secret key

--- backend/main-api/app.py
+++ backend/main-api/app.py
@@ -1,10 +1,12 @@
 #!venv/bin/python
+import secrets
 from flask import Flask
 
 from blueprints.archive import archive
 from blueprints.auth import auth
 
 app = Flask(__name__)
+app.config["SECRET_KEY"] = secrets.token_hex()
 
 app.register_blueprint(archive, url_prefix="/archive")
 app.register_blueprint(auth, url_prefix="/auth")
